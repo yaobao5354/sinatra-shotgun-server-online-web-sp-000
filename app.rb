24a1e6ee81b433eb31c3c36b2ad5b805228918e6
yaoyu bao <yaobao5354@gmail.com>
2020-03-10 19:23:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+
+  get '/' do
+    "Started my server using Shotgun!"
   end
 
 end
